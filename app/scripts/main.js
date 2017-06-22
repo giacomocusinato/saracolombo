82810a1c0fb0f6dd26387e7bf984458cac4f00c6
giacomocusinato <giacomo.cusinato@outlook.com>
2017-06-22 19:27:47 +0200

    First website prototype

---
+++ app/scripts/main.js
@@ -0,0 +1,278 @@
+$(function() {
+  'use strict';
+
+
+  /*----------- Global variables -----------*/
+
+
+  let projects = $('.container');
+  let currentProject = 1;
+  let rightProject = 2;
+  let leftProject = 0;
+  let animationOnGoing = false;
+  let projectOpened = false;
+  let touchStart;
+
+
+
+  /*----------- Inits -----------*/
+
+
+  manageUrlParams();
+
+  $('.project-title span').hide();
+  $(projects[currentProject])
+    .find('.project-title .first-line')
+    .show()
+    .addClass('animated slideInDown');
+
+
+  /*----------- Event listeners -----------*/
+
+
+  $(window).on('touchstart', function(e) {
+    touchStart = e.originalEvent.touches[0].clientX;
+  });
+
+  $(window).on('touchend', function(e) {
+    let touchEnd = e.originalEvent.changedTouches[0].clientX;
+    if (touchStart > touchEnd + 5) {
+      swipeProject('right');
+    } else if (touchStart < touchEnd - 5) {
+      swipeProject('left');
+    }
+  });
+
+  $(window).on('wheel', function(e) {
+    let deltaY = e.originalEvent.deltaY;
+    let deltaX = e.originalEvent.deltaX;
+
+    if (
+      deltaY > 1 || deltaY < -1 ||
+      deltaX > 1 || deltaX < -1
+    ) return;
+
+    swipeProject(deltaY > 0 || deltaX > 0 ? 'right' : 'left')
+  });
+
+  $('.project-title').click(function() {
+    let p = $(this).attr('data-open');
+    window.history.replaceState("page" + p, p, "?p=" + p);
+    openProject(getUrlParam('p'));
+  });
+
+  $('.project-title').hover(function() {
+    if (projectOpened) return;
+
+    $(projects)
+      .animate({
+        'width': '-=20px',
+        'height': '-=20px',
+        'margin-left': '+=10px',
+        'margin-top': '+=10px'
+      }, 400);
+  }, function() {
+    if (projectOpened) return;
+
+    $(projects)
+      .animate({
+        'width': '+=20px',
+        'height': '+=20px',
+        'margin-left': '-=10px',
+        'margin-top': '-=10px'
+      }, 400)
+
+  });
+
+  $('.top-link.left').click(function() {
+    closeProject();
+  });
+
+
+
+  /*----------- Behaviour functions -----------*/
+
+
+  function swipeProject(direction) {
+    if (animationOnGoing || projectOpened)
+      return;
+
+    animationOnGoing = true;
+    $(projects).css('z-index', '-1');
+
+    if (direction == 'right') {
+      $(projects[currentProject]).animate({
+        'left': '-=50%'
+      }, 2000, function() {
+        leftProject = currentProject;
+        currentProject = rightProject;
+        rightProject = calcNext(currentProject);
+
+        swipeEnded();
+      });
+
+      $(projects[rightProject]).css('z-index', '0').animate({
+        'left': '-=100%'
+      }, 2000);
+    } else {
+      $(projects[currentProject]).show().animate({
+        'left': '+=50%'
+      }, 2000, function() {
+        rightProject = currentProject;
+        currentProject = leftProject;
+        leftProject = calcPrevious(currentProject);
+
+        swipeEnded();
+      });
+
+      $(projects[leftProject]).css('z-index', '0').animate({
+        'left': '+=100%'
+      }, 2000);
+    }
+  }
+
+  function swipeEnded() {
+    $('.project-title span').hide();
+    $(projects[currentProject])
+      .find('.project-title .first-line')
+      .fadeIn()
+      .addClass('animated slideInDown');
+    $(projects[currentProject])
+      .find('.project-title .second-line')
+      .fadeIn()
+      .addClass('animated slideInUp');
+
+    $(projects[rightProject]).css('left', '+100%');
+    $(projects[leftProject]).css('left', '-100%');
+    animationOnGoing = false;
+  }
+
+  function openProject(name) {
+    if (projectOpened || animationOnGoing) return;
+
+    projectOpened = true;
+    animationOnGoing = true;
+    $('.container').hide();
+
+
+    $(projects[currentProject])
+      .show()
+      .css('position', 'relative')
+      .animate({
+        'width': '-=160px',
+        'height': '-=120px',
+        'margin-top': '+=100px',
+        'margin-left': '+=70px'
+      }, 2000, function() {
+        $('.project-title')
+          .addClass('active')
+          .animate({
+            'margin-top': '-=62%'
+          }, 500);
+        $('body').css('overflow', 'auto');
+        $('.top-link').css('color', '#333');
+        $('.top-link.left').fadeIn();
+        $('section.project.' + name).fadeIn();
+        animationOnGoing = false;
+      });
+  }
+
+  function closeProject() {
+    if (!projectOpened || animationOnGoing) return;
+
+    animationOnGoing = true;
+    $('section.project').fadeOut();
+    $('.container').show();
+    $('.project-title')
+      .delay(400)
+      .animate({
+        'margin-top': '0'
+      }, 2000, function() {
+        $(this).removeClass('active');
+      });
+    $(projects[currentProject])
+      .delay(400)
+      .animate({
+        'width': '100%',
+        'height': '100%',
+        'margin': '0'
+      }, 2000, function() {
+        $(this).css('position', 'absolute');
+        $('.top-link').css('color', 'white');
+        $('.top-link.left').fadeOut();
+        animationOnGoing = false;
+        projectOpened = false;
+
+
+        resetContainersSize();
+
+        window.history.replaceState("home", "Sara Colombo", "/")
+      })
+    $('body').css('overflow', 'hidden');
+  }
+
+  function resetContainersSize() {
+    $(projects).css({
+      'width': '100%',
+      'height': '100%',
+      'margin-top': '0',
+      'margin-left': '0'
+    });
+  }
+
+  function manageUrlParams() {
+    let p = getUrlParam('p');
+    if (p) {
+      let projectFoud = false;
+      $(projects).each(function(i, item) {
+        let projectName = $(item).children('h1.project-title').attr('data-open');
+        if (projectName == p) {
+          projectFoud = true;
+          currentProject = i;
+          leftProject = calcPrevious(currentProject);
+          rightProject = calcNext(currentProject);
+          $(projects[currentProject]).css('left', '0');
+          $(projects[rightProject]).css('left', '+100%');
+          $(projects[leftProject]).css('left', '-100%');
+          $(projects).show();
+
+          openProject(projectName);
+        }
+      });
+
+      if (!projectFoud) {
+        $(projects).show();
+      }
+    } else {
+      $(projects).show();
+    }
+
+  }
+
+
+
+  /*----------- Utility functions -----------*/
+
+
+  function getUrlParam(field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+  };
+
+  function calcNext(i) {
+    if (i == projects.length - 1)
+      return 0;
+    else
+      return i + 1;
+  }
+
+  function calcPrevious(i) {
+    if (i == 0)
+      return projects.length - 1;
+    else
+      return i - 1;
+  }
+
+});
