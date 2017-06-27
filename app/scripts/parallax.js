aa3bb9dd12423c721309d4b3580fdede1cb04f23
giacomocusinato <giacomo.cusinato@outlook.com>
2017-06-27 15:24:32 +0200

    Improve parallax.js

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,77 +1,27 @@
 $(function() {
+
   let controller = new ScrollMagic.Controller();
 
 
-  // Images scenes
+  /*----------- Sperduta scenes -----------*/
 
-  new ScrollMagic.Scene({
-      triggerElement: '.section-1',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-2 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-      triggerElement: '.section-2',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-3 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-      triggerElement: '.section-3',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-4 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-      triggerElement: '.section-4',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-5 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-      triggerElement: '.section-5',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-6 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-      triggerElement: '.section-6',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.section-7 .image', 1, {
-      backgroundPosition: '50% 100%',
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
-
-  // Box scenes
+  $('.sperduta .row').each(function(i) {
+    new ScrollMagic.Scene({
+        triggerElement: '.sperduta .section-' + (i + 1),
+        duration: 1500,
+      })
+      .setTween(TweenMax.to('.sperduta .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
+        ease: Linear.easeNone
+      }))
+      .addTo(controller);
+  });
 
   new ScrollMagic.Scene({
       triggerElement: '.sperduta .section-2',
       duration: 1500,
     })
-    .setTween(TweenMax.to('.section-3 .box', 1, {
+    .setTween(TweenMax.to('.sperduta .section-3 .box', 1, {
       css: {
         top: '+=40px'
       },
@@ -80,7 +30,7 @@ $(function() {
     .addTo(controller);
 
   new ScrollMagic.Scene({
-      triggerElement: '.section-5',
+      triggerElement: '.sperduta .section-5',
       duration: 800,
     })
     .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
@@ -91,15 +41,31 @@ $(function() {
     }))
     .addTo(controller);
 
+
+
+  /*----------- Roots scenes -----------*/
+
+  $('.roots .row').each(function(i) {
     new ScrollMagic.Scene({
-        triggerElement: '.section-1',
-        duration: 1200,
+        triggerElement: '.roots .section-' + (i + 1),
+        duration: 1500,
       })
-      .setTween(TweenMax.to('.roots .section-2 .box', 1, {
-        css: {
-          bottom: '+=220px'
-        },
+      .setTween(TweenMax.to('.roots .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
         ease: Linear.easeNone
       }))
       .addTo(controller);
+  });
+
+  new ScrollMagic.Scene({
+      triggerElement: '.roots .section-1',
+      duration: 1200,
+    })
+    .setTween(TweenMax.to('.roots .section-2 .box', 1, {
+      css: {
+        bottom: '+=220px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
 });
