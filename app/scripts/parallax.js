4241a25929fdda725a2843d07226685716a3b400
giacomocusinato <giacomo.cusinato@outlook.com>
2017-06-27 12:01:28 +0200

    Add project roots, genral improvements

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -2,7 +2,7 @@ $(function() {
   let controller = new ScrollMagic.Controller();
 
 
-  // Sperduta project screnes
+  // Images scenes
 
   new ScrollMagic.Scene({
       triggerElement: '.section-1',
@@ -25,22 +25,20 @@ $(function() {
     .addTo(controller);
 
   new ScrollMagic.Scene({
-      triggerElement: '.section-2',
+      triggerElement: '.section-3',
       duration: 1500,
     })
-    .setTween(TweenMax.to('.section-3 .box', 1, {
-      css: {
-        top: '+=40px'
-      },
+    .setTween(TweenMax.to('.section-4 .image', 1, {
+      backgroundPosition: '50% 100%',
       ease: Linear.easeNone
     }))
     .addTo(controller);
 
   new ScrollMagic.Scene({
       triggerElement: '.section-4',
-      duration: 800,
+      duration: 1500,
     })
-    .setTween(TweenMax.to('.section-4 .image', 1, {
+    .setTween(TweenMax.to('.section-5 .image', 1, {
       backgroundPosition: '50% 100%',
       ease: Linear.easeNone
     }))
@@ -57,14 +55,51 @@ $(function() {
     .addTo(controller);
 
   new ScrollMagic.Scene({
+      triggerElement: '.section-6',
+      duration: 1500,
+    })
+    .setTween(TweenMax.to('.section-7 .image', 1, {
+      backgroundPosition: '50% 100%',
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+
+
+  // Box scenes
+
+  new ScrollMagic.Scene({
+      triggerElement: '.sperduta .section-2',
+      duration: 1500,
+    })
+    .setTween(TweenMax.to('.section-3 .box', 1, {
+      css: {
+        top: '+=40px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
       triggerElement: '.section-5',
       duration: 800,
     })
-    .setTween(TweenMax.to('.section-6 .box', 1, {
+    .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
       css: {
         bottom: '+=48px'
       },
       ease: Linear.easeNone
     }))
     .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: '.section-1',
+        duration: 1200,
+      })
+      .setTween(TweenMax.to('.roots .section-2 .box', 1, {
+        css: {
+          bottom: '+=220px'
+        },
+        ease: Linear.easeNone
+      }))
+      .addTo(controller);
 });
