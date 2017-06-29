bc262abc2722bc1421be0d1cd82a9929a36e2919
giacomocusinato <giacomo.cusinato@outlook.com>
2017-06-29 10:31:33 +0200

    Move projects in html partials folder

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,88 +1,130 @@
 $(function() {
 
   let controller = new ScrollMagic.Controller();
-  
+
+
+  $('.project.sperduta').load('partials/sperduta.html', function() {
+    loadSperdutaScenes();
+  });
+
+  $('.project.roots').load('partials/roots.html', function() {
+    loadRootsScenes();
+  });
+
+  $('.project.firefighters').load('partials/firefighters.html', function() {
+    loadFirefightersScenes();
+  });
+
+  $('.project.black').load('partials/black.html', function() {
+    loadBlackScenes();
+  });
+
+  $('.project.uppercut').load('partials/uppercut.html', function() {
+    loadUppercutScenes();
+  });
+
 
   /*----------- Sperduta scenes -----------*/
 
-  $('.sperduta .row').each(function(i) {
+  function loadSperdutaScenes() {
+    $('.sperduta .row').each(function(i) {
+      new ScrollMagic.Scene({
+          triggerElement: '.sperduta .section-' + (i + 1),
+          duration: 1500,
+        })
+        .setTween(TweenMax.to('.sperduta .section-' + (i + 2) + ' .image', 1, {
+          backgroundPosition: '50% 100%',
+          ease: Linear.easeNone
+        }))
+        .addTo(controller);
+    });
+
     new ScrollMagic.Scene({
-        triggerElement: '.sperduta .section-' + (i + 1),
+        triggerElement: '.sperduta .section-2',
         duration: 1500,
       })
-      .setTween(TweenMax.to('.sperduta .section-' + (i + 2) + ' .image', 1, {
-        backgroundPosition: '50% 100%',
+      .setTween(TweenMax.to('.sperduta .section-3 .box', 1, {
+        css: {
+          top: '+=40px'
+        },
         ease: Linear.easeNone
       }))
       .addTo(controller);
-  });
 
-  new ScrollMagic.Scene({
-      triggerElement: '.sperduta .section-2',
-      duration: 1500,
-    })
-    .setTween(TweenMax.to('.sperduta .section-3 .box', 1, {
-      css: {
-        top: '+=40px'
-      },
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '.sperduta .section-5',
+        duration: 800,
+      })
+      .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
+        css: {
+          bottom: '+=48px'
+        },
+        ease: Linear.easeNone
+      }))
+      .addTo(controller);
+  }
 
-  new ScrollMagic.Scene({
-      triggerElement: '.sperduta .section-5',
-      duration: 800,
-    })
-    .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
-      css: {
-        bottom: '+=48px'
-      },
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
 
 
 
   /*----------- Roots scenes -----------*/
 
-  $('.roots .row').each(function(i) {
+  function loadRootsScenes() {
+    $('.roots .row').each(function(i) {
+      new ScrollMagic.Scene({
+          triggerElement: '.roots .section-' + (i + 1),
+          duration: 1500,
+        })
+        .setTween(TweenMax.to('.roots .section-' + (i + 2) + ' .image', 1, {
+          backgroundPosition: '50% 100%',
+          ease: Linear.easeNone
+        }))
+        .addTo(controller);
+    });
+
     new ScrollMagic.Scene({
-        triggerElement: '.roots .section-' + (i + 1),
-        duration: 1500,
+        triggerElement: '.roots .section-1',
+        duration: 1200,
       })
-      .setTween(TweenMax.to('.roots .section-' + (i + 2) + ' .image', 1, {
-        backgroundPosition: '50% 100%',
+      .setTween(TweenMax.to('.roots .section-2 .box', 1, {
+        css: {
+          bottom: '+=220px'
+        },
         ease: Linear.easeNone
       }))
       .addTo(controller);
-  });
-
-  new ScrollMagic.Scene({
-      triggerElement: '.roots .section-1',
-      duration: 1200,
-    })
-    .setTween(TweenMax.to('.roots .section-2 .box', 1, {
-      css: {
-        bottom: '+=220px'
-      },
-      ease: Linear.easeNone
-    }))
-    .addTo(controller);
-
+  }
 
 
   /*----------- Firefighters scenes -----------*/
 
-  $('.firefighters .row').each(function(i) {
-    new ScrollMagic.Scene({
-        triggerElement: '.firefighters .section-' + (i + 1),
-        duration: 1500,
-      })
-      .setTween(TweenMax.to('.firefighters .section-' + (i + 2) + ' .image', 1, {
-        backgroundPosition: '50% 100%',
-        ease: Linear.easeNone
-      }))
-      .addTo(controller);
-  });
+  function loadFirefightersScenes() {
+    $('.firefighters .row').each(function(i) {
+      new ScrollMagic.Scene({
+          triggerElement: '.firefighters .section-' + (i + 1),
+          duration: 1500,
+        })
+        .setTween(TweenMax.to('.firefighters .section-' + (i + 2) + ' .image', 1, {
+          backgroundPosition: '50% 100%',
+          ease: Linear.easeNone
+        }))
+        .addTo(controller);
+    });
+  }
+
+
+  /*----------- Black scenes -----------*/
+
+  function loadBlackScenes() {
+
+  }
+
+
+  /*----------- Uppercut scenes -----------*/
+
+  function loadUppercutScenes() {
+
+  }
+
 
 });
