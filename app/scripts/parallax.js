b133c5d9d51b2cd49f8fdf3f409412be450cc588
giacomocusinato <giacomo.cusinato@outlook.com>
2017-07-01 11:17:49 +0200

    Testing clojures

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,162 +1,157 @@
-$(function() {
+let controller = new ScrollMagic.Controller();
 
-  let controller = new ScrollMagic.Controller();
-
-  loadSperdutaScenes();
-  loadRootsScenes();
-  loadFirefightersScenes();
-  loadBlackScenes();
-  loadUppercutScenes();
+loadSperdutaScenes();
+loadRootsScenes();
+loadFirefightersScenes();
+loadBlackScenes();
+loadUppercutScenes();
 
 
-  /*----------- Sperduta scenes -----------*/
+/*----------- Sperduta scenes -----------*/
 
-  function loadSperdutaScenes() {
-    $('.sperduta .row').each(function(i) {
-      new ScrollMagic.Scene({
-          triggerElement: '.sperduta .section-' + (i + 1),
-          duration: 1500,
-        })
-        .setTween(TweenMax.to('.sperduta .section-' + (i + 2) + ' .image', 1, {
-          backgroundPosition: '50% 100%',
-          ease: Linear.easeNone
-        }))
-        .addTo(controller);
-    });
-
+function loadSperdutaScenes() {
+  $('.sperduta .row').each(function(i) {
     new ScrollMagic.Scene({
-        triggerElement: '.sperduta .section-2',
+        triggerElement: '.sperduta .section-' + (i + 1),
         duration: 1500,
       })
-      .setTween(TweenMax.to('.sperduta .section-3 .box', 1, {
-        css: {
-          top: '+=40px'
-        },
+      .setTween(TweenMax.to('.sperduta .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
         ease: Linear.easeNone
       }))
       .addTo(controller);
+  });
 
+  new ScrollMagic.Scene({
+      triggerElement: '.sperduta .section-2',
+      duration: 1500,
+    })
+    .setTween(TweenMax.to('.sperduta .section-3 .box', 1, {
+      css: {
+        top: '+=40px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+      triggerElement: '.sperduta .section-5',
+      duration: 800,
+    })
+    .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
+      css: {
+        bottom: '+=48px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+}
+
+
+
+
+/*----------- Roots scenes -----------*/
+
+function loadRootsScenes() {
+  $('.roots .row').each(function(i) {
     new ScrollMagic.Scene({
-        triggerElement: '.sperduta .section-5',
-        duration: 800,
-      })
-      .setTween(TweenMax.to('.sperduta .section-6 .box', 1, {
-        css: {
-          bottom: '+=48px'
-        },
-        ease: Linear.easeNone
-      }))
-      .addTo(controller);
-  }
-
-
-
-
-  /*----------- Roots scenes -----------*/
-
-  function loadRootsScenes() {
-    $('.roots .row').each(function(i) {
-      new ScrollMagic.Scene({
-          triggerElement: '.roots .section-' + (i + 1),
-          duration: 1500,
-        })
-        .setTween(TweenMax.to('.roots .section-' + (i + 2) + ' .image', 1, {
-          backgroundPosition: '50% 100%',
-          ease: Linear.easeNone
-        }))
-        .addTo(controller);
-    });
-
-    new ScrollMagic.Scene({
-        triggerElement: '.roots .section-1',
-        duration: 1200,
-      })
-      .setTween(TweenMax.to('.roots .section-2 .box', 1, {
-        css: {
-          bottom: '+=220px'
-        },
-        ease: Linear.easeNone
-      }))
-      .addTo(controller);
-  }
-
-
-
-  /*----------- Firefighters scenes -----------*/
-
-  function loadFirefightersScenes() {
-    $('.firefighters .row').each(function(i) {
-      new ScrollMagic.Scene({
-          triggerElement: '.firefighters .section-' + (i + 1),
-          duration: 1500,
-        })
-        .setTween(TweenMax.to('.firefighters .section-' + (i + 2) + ' .image', 1, {
-          backgroundPosition: '50% 100%',
-          ease: Linear.easeNone
-        }))
-        .addTo(controller);
-    });
-  }
-
-
-
-  /*----------- Black scenes -----------*/
-
-  function loadBlackScenes() {
-    $('.black .row').each(function(i) {
-      new ScrollMagic.Scene({
-          triggerElement: '.black .section-' + (i + 1),
-          duration: 1500,
-        })
-        .setTween(TweenMax.to('.black .section-' + (i + 2) + ' .image', 1, {
-          backgroundPosition: '50% 100%',
-          ease: Linear.easeNone
-        }))
-        .addTo(controller);
-    });
-  }
-
-
-
-  /*----------- Uppercut scenes -----------*/
-
-  function loadUppercutScenes() {
-    $('.uppercut .row').each(function(i) {
-      new ScrollMagic.Scene({
-          triggerElement: '.uppercut .section-' + (i + 1),
-          duration: 1500,
-        })
-        .setTween(TweenMax.to('.uppercut .section-' + (i + 2) + ' .image', 1, {
-          backgroundPosition: '50% 100%',
-          ease: Linear.easeNone
-        }))
-        .addTo(controller);
-    });
-
-    new ScrollMagic.Scene({
-        triggerElement: '.uppercut .section-4',
+        triggerElement: '.roots .section-' + (i + 1),
         duration: 1500,
       })
-      .setTween(TweenMax.to('.uppercut .section-5 .box', 1, {
-        css: {
-          top: '+=48px'
-        },
+      .setTween(TweenMax.to('.roots .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
         ease: Linear.easeNone
       }))
       .addTo(controller);
+  });
 
+  new ScrollMagic.Scene({
+      triggerElement: '.roots .section-1',
+      duration: 1200,
+    })
+    .setTween(TweenMax.to('.roots .section-2 .box', 1, {
+      css: {
+        bottom: '+=220px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+}
+
+
+
+/*----------- Firefighters scenes -----------*/
+
+function loadFirefightersScenes() {
+  $('.firefighters .row').each(function(i) {
     new ScrollMagic.Scene({
-        triggerElement: '.uppercut .section-6',
+        triggerElement: '.firefighters .section-' + (i + 1),
         duration: 1500,
       })
-      .setTween(TweenMax.to('.uppercut .section-7 .box', 1, {
-        css: {
-          top: '+=48px'
-        },
+      .setTween(TweenMax.to('.firefighters .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
         ease: Linear.easeNone
       }))
       .addTo(controller);
-  }
+  });
+}
 
 
-});
+
+/*----------- Black scenes -----------*/
+
+function loadBlackScenes() {
+  $('.black .row').each(function(i) {
+    new ScrollMagic.Scene({
+        triggerElement: '.black .section-' + (i + 1),
+        duration: 1500,
+      })
+      .setTween(TweenMax.to('.black .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
+        ease: Linear.easeNone
+      }))
+      .addTo(controller);
+  });
+}
+
+
+
+/*----------- Uppercut scenes -----------*/
+
+function loadUppercutScenes() {
+  $('.uppercut .row').each(function(i) {
+    new ScrollMagic.Scene({
+        triggerElement: '.uppercut .section-' + (i + 1),
+        duration: 1500,
+      })
+      .setTween(TweenMax.to('.uppercut .section-' + (i + 2) + ' .image', 1, {
+        backgroundPosition: '50% 100%',
+        ease: Linear.easeNone
+      }))
+      .addTo(controller);
+  });
+
+  new ScrollMagic.Scene({
+      triggerElement: '.uppercut .section-4',
+      duration: 1500,
+    })
+    .setTween(TweenMax.to('.uppercut .section-5 .box', 1, {
+      css: {
+        top: '+=48px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+      triggerElement: '.uppercut .section-6',
+      duration: 1500,
+    })
+    .setTween(TweenMax.to('.uppercut .section-7 .box', 1, {
+      css: {
+        top: '+=48px'
+      },
+      ease: Linear.easeNone
+    }))
+    .addTo(controller);
+}
--- app/scripts/sliders.js
+++ app/scripts/sliders.js
@@ -1,162 +1,160 @@
-$(function() {
-  'use strict';
+'use strict';
 
-  function slider(container) {
-    this.leftImage = 0;
-    this.currentImage = 1;
-    this.rightImage = 2;
-    this.container = container;
-  };
+function slider(container) {
+  this.leftImage = 0;
+  this.currentImage = 1;
+  this.rightImage = 2;
+  this.container = container;
+};
 
-  let sliderOne = new slider($('.sperduta .section-2 .slider'));
-  let sliderTwo = new slider($('.sperduta .section-3 .slider'));
+let sliderOne = new slider($('.sperduta .section-2 .slider'));
+let sliderTwo = new slider($('.sperduta .section-3 .slider'));
 
-  let sliderThree = new slider($('.firefighters .section-3 .slider'));
-  let sliderFourLeft = new slider($('.firefighters .section-6 .slider.left'));
-  let sliderFourRight = new slider($('.firefighters .section-6 .slider.right'));
-  let sliderFive = new slider($('.firefighters .section-10 .slider'));
-  let sliderSix = new slider($('.firefighters .section-12 .slider'));
+let sliderThree = new slider($('.firefighters .section-3 .slider'));
+let sliderFourLeft = new slider($('.firefighters .section-6 .slider.left'));
+let sliderFourRight = new slider($('.firefighters .section-6 .slider.right'));
+let sliderFive = new slider($('.firefighters .section-10 .slider'));
+let sliderSix = new slider($('.firefighters .section-12 .slider'));
 
-  let sliderSeven = new slider($('.uppercut .section-8 .slider'));
+let sliderSeven = new slider($('.uppercut .section-8 .slider'));
 
-  let sliderEightLeft = new slider($('.black .section-2 .slider.left'));
-  let sliderEightRight = new slider($('.black .section-2 .slider.right'));
-  let sliderNine = new slider($('.black .section-5 .slider'));
-  let sliderTen = new slider($('.black .section-8 .slider'));
-  let sliderEleven = new slider($('.black .section-9 .slider'));
-
-
-  let animationOnGoing = false;
+let sliderEightLeft = new slider($('.black .section-2 .slider.left'));
+let sliderEightRight = new slider($('.black .section-2 .slider.right'));
+let sliderNine = new slider($('.black .section-5 .slider'));
+let sliderTen = new slider($('.black .section-8 .slider'));
+let sliderEleven = new slider($('.black .section-9 .slider'));
 
 
-  $('.sperduta .section-2 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderOne, 'left') : swipe(sliderOne, 'right')
-  });
-  $('.sperduta .section-3 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderTwo, 'left') : swipe(sliderTwo, 'right')
-  });
-  $('.firefighters .section-3 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderThree, 'left') : swipe(sliderThree, 'right')
-  });
-  $('.firefighters .section-6 .slider-nav').click(function() {
-    if ($(this).hasClass('right')) {
-      swipe(sliderFourLeft, 'left', true);
-      swipe(sliderFourRight, 'left', true);
-      animationOnGoing = true;
-    } else {
-      swipe(sliderFourLeft, 'right', true);
-      swipe(sliderFourRight, 'right', true);
-      animationOnGoing = true;
-    }
-
-  });
-  $('.firefighters .section-10 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderFive, 'left') : swipe(sliderFive, 'right')
-  });
-  $('.firefighters .section-12 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderSix, 'left') : swipe(sliderSix, 'right')
-  });
-  $('.uppercut .section-8 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderSeven, 'left') : swipe(sliderSeven, 'right')
-  });
-  $('.black .section-2 .slider-nav').click(function() {
-    if ($(this).hasClass('right')) {
-      swipe(sliderEightLeft, 'left', true);
-      swipe(sliderEightRight, 'left', true);
-      animationOnGoing = true;
-    } else {
-      swipe(sliderEightLeft, 'right', true);
-      swipe(sliderEightRight, 'right', true);
-      animationOnGoing = true;
-    }
-
-  });
-  $('.black .section-5 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderNine, 'left') : swipe(sliderNine, 'right')
-  });
-  $('.black .section-8 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderTen, 'left') : swipe(sliderTen, 'right')
-  });
-  $('.black .section-9 .slider-nav').click(function() {
-    $(this).hasClass('right') ?
-      swipe(sliderEleven, 'left') : swipe(sliderEleven, 'right')
-  });
+let animationOnGoing = false;
 
 
-
-  $('.slider').hover(function() {
-    $(this)
-      .children('.slider-nav.left')
-      .fadeIn()
-      .addClass('animated slideInRight');
-    $(this)
-      .children('.slider-nav.right')
-      .fadeIn()
-      .addClass('animated slideInLeft');
-  }, function() {
-    $(this).children('.slider-nav').fadeOut();
-  });
-
-  function swipe(slider, direction, isDoubleSlider = false) {
-    if (animationOnGoing) return;
-    if (!isDoubleSlider)
-      animationOnGoing = true;
-
-    let imageToSlide, currentImageVelocity;
-    if (direction == 'left') {
-      imageToSlide = slider.rightImage;
-      currentImageVelocity = '-50%';
-    } else {
-      imageToSlide = slider.leftImage;
-      currentImageVelocity = '+50%';
-    }
-
-    let images = slider.container.find('.image');
-
-    $(images)
-      .eq(slider.currentImage)
-      .css('z-index', '0')
-      .animate({
-        'left': currentImageVelocity
-      }, 1200);
-    $(images)
-      .eq(imageToSlide)
-      .css('z-index', '1')
-      .animate({
-        'left': '0'
-      }, 1200, function() {
-        slider.currentImage = imageToSlide;
-        slider.rightImage = calcNext(imageToSlide, images);
-        slider.leftImage = calcPrevious(imageToSlide, images);
-
-        images.eq(slider.leftImage).css('left', '-100%');
-        images.eq(slider.rightImage).css('left', '100%');
-
-        console.log(slider);
-        animationOnGoing = false;
-      });
-  }
-
-
-  function calcNext(i, array) {
-    if (i == array.length - 1)
-      return 0;
-    else
-      return i + 1;
-  }
-
-  function calcPrevious(i, array) {
-    if (i == 0)
-      return array.length - 1;
-    else
-      return i - 1;
-  }
+$('.sperduta .section-2 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderOne, 'left') : swipe(sliderOne, 'right')
 });
+$('.sperduta .section-3 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderTwo, 'left') : swipe(sliderTwo, 'right')
+});
+$('.firefighters .section-3 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderThree, 'left') : swipe(sliderThree, 'right')
+});
+$('.firefighters .section-6 .slider-nav').click(function() {
+  if ($(this).hasClass('right')) {
+    swipe(sliderFourLeft, 'left', true);
+    swipe(sliderFourRight, 'left', true);
+    animationOnGoing = true;
+  } else {
+    swipe(sliderFourLeft, 'right', true);
+    swipe(sliderFourRight, 'right', true);
+    animationOnGoing = true;
+  }
+
+});
+$('.firefighters .section-10 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderFive, 'left') : swipe(sliderFive, 'right')
+});
+$('.firefighters .section-12 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderSix, 'left') : swipe(sliderSix, 'right')
+});
+$('.uppercut .section-8 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderSeven, 'left') : swipe(sliderSeven, 'right')
+});
+$('.black .section-2 .slider-nav').click(function() {
+  if ($(this).hasClass('right')) {
+    swipe(sliderEightLeft, 'left', true);
+    swipe(sliderEightRight, 'left', true);
+    animationOnGoing = true;
+  } else {
+    swipe(sliderEightLeft, 'right', true);
+    swipe(sliderEightRight, 'right', true);
+    animationOnGoing = true;
+  }
+
+});
+$('.black .section-5 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderNine, 'left') : swipe(sliderNine, 'right')
+});
+$('.black .section-8 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderTen, 'left') : swipe(sliderTen, 'right')
+});
+$('.black .section-9 .slider-nav').click(function() {
+  $(this).hasClass('right') ?
+    swipe(sliderEleven, 'left') : swipe(sliderEleven, 'right')
+});
+
+
+
+$('.slider').hover(function() {
+  $(this)
+    .children('.slider-nav.left')
+    .fadeIn()
+    .addClass('animated slideInRight');
+  $(this)
+    .children('.slider-nav.right')
+    .fadeIn()
+    .addClass('animated slideInLeft');
+}, function() {
+  $(this).children('.slider-nav').fadeOut();
+});
+
+function swipe(slider, direction, isDoubleSlider = false) {
+  if (animationOnGoing) return;
+  if (!isDoubleSlider)
+    animationOnGoing = true;
+
+  let imageToSlide, currentImageVelocity;
+  if (direction == 'left') {
+    imageToSlide = slider.rightImage;
+    currentImageVelocity = '-50%';
+  } else {
+    imageToSlide = slider.leftImage;
+    currentImageVelocity = '+50%';
+  }
+
+  let images = slider.container.find('.image');
+
+  $(images)
+    .eq(slider.currentImage)
+    .css('z-index', '0')
+    .animate({
+      'left': currentImageVelocity
+    }, 1200);
+  $(images)
+    .eq(imageToSlide)
+    .css('z-index', '1')
+    .animate({
+      'left': '0'
+    }, 1200, function() {
+      slider.currentImage = imageToSlide;
+      slider.rightImage = calcNext(imageToSlide, images);
+      slider.leftImage = calcPrevious(imageToSlide, images);
+
+      images.eq(slider.leftImage).css('left', '-100%');
+      images.eq(slider.rightImage).css('left', '100%');
+
+      console.log(slider);
+      animationOnGoing = false;
+    });
+}
+
+
+function calcNext(i, array) {
+  if (i == array.length - 1)
+    return 0;
+  else
+    return i + 1;
+}
+
+function calcPrevious(i, array) {
+  if (i == 0)
+    return array.length - 1;
+  else
+    return i - 1;
+}
